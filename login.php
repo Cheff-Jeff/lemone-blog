9f67bb7d920a713f70ffc2afde16c8a57ddaa377
CheffJeff <1999jeffreynijkamp@gmail.com>
2025-08-22 12:49:50 +0200

    frontend login en registreren

--- login.php
+++ login.php
@@ -1,9 +1,28 @@
+<?php $css = "login"; ?>
 <?php require_once __DIR__ . "/Templates/Header.php" ?>
+<section class="main-page">
+    <div class="container">
+        <h1 class="title">Login</h1>
 
-<form action="./Backend/Login.php" method="post">
-            <input type="email" placeholder="email" name="email">
-            <input type="password" placeholder="Wachtwoord" name="password">
+        <form action="./Backend/Login.php" method="post">
+            <input 
+                type="email"
+                placeholder="email"
+                name="email"
+                class="input-field"
+                required
+            >
 
-            <button type="submit">Login</button>
+            <input 
+                type="password"
+                placeholder="Wachtwoord"
+                name="password"
+                class="input-field"
+                required
+            >
+
+            <button type="submit" class="btn-primary">Login</button>
         </form>
+    </div>
+</section>
 <?php require_once __DIR__ . "/Templates/Footer.php" ?>
--- register.php
+++ register.php
@@ -1,10 +1,28 @@
+<?php $css = "login"; ?>
 <?php require_once __DIR__ . "/Templates/Header.php" ?>
+<section class="main-page">
+    <div class="container">
+        <h1 class="title">Registreren</h1>
 
-    <form action="./Backend/Register.php" method="post">
-        <input type="email" placeholder="email" name="email">
-        <input type="password" placeholder="Wachtwoord" name="password">
+        <form action="./Backend/Register.php" method="post">
+            <input 
+                type="email"
+                placeholder="email"
+                name="email"
+                class="input-field"
+                required
+            >
 
-        <button type="submit">Registreren</button>
-    </form>
+            <input 
+                type="password"
+                placeholder="Wachtwoord"
+                name="password"
+                class="input-field"
+                required
+            >
 
+            <button type="submit" class="btn-primary">Registreren</button>
+        </form>
+    </div>
+</section>
 <?php require_once __DIR__ . "/Templates/Footer.php" ?>
