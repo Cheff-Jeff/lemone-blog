91e93c077b6f5e41698e1ffbcbc369a05f248875
CheffJeff <1999jeffreynijkamp@gmail.com>
2025-08-23 16:48:52 +0200

    fix copy past error

--- Backend/EditPost.php
+++ Backend/EditPost.php
@@ -7,16 +7,16 @@ use PHP\Helpers\SessionController;
 
 if (empty($_POST['title']) || empty($_POST['content']) || empty($_POST['id']))
 {
-    header('Location: ../edit-post.php?error=1&postID='.$_GET['postID']);
+    header('Location: ../edit-post.php?error=1&postID='.$_POST['id']);
 }
 
 try {
     if (!SessionController::isLoggedIn()) throw new Exception("Gebruiker niet ingelogd");
-    if (!is_numeric($_GET['id'])) throw new Exception("Id is geen number");
+    if (!is_numeric($_POST['id'])) throw new Exception("Id is geen number");
     if (!is_string($_POST['title'])) throw new Exception("Title is geen string");
     if (!is_string($_POST['content'])) throw new Exception("Content is geen string");
 
-    $id = intval($_GET['id']);
+    $id = intval($_POST['id']);
     if (!SessionController::postBelongsToUser($id)) throw new Exception("De post is niet van u.");
 
     $title = strip_tags($_POST['title']);
@@ -31,5 +31,5 @@ try {
 
     header('Location: ../post.php?postID='.$id);
 } catch (\Throwable $th) {
-    header('Location: ../edit-post.php?error=1&postID='.$_GET['postID']);
+    header('Location: ../edit-post.php?error=1&postID='.$_POST['id']);
 }
